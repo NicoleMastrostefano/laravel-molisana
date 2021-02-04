77fcdf904bce5069f5cc264d5316afddf29c32b0
Nicole Mastrostefano <nicole_mas@hotmail.it>
2021-02-04 23:17:09 +0100

    config db

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -6,7 +6,7 @@
           <h1>{{ $product["titolo"] }}</h1>
           <img src="{{ $product['src-h']}}" alt="">
           <img src="{{ $product['src-p']}}" alt="">
-          <p> {{ $product["descrizione"] }}</p>
+          <p> {!! $product["descrizione"] !!}</p>
         </div>
       </div>
     </main>
